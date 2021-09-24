03e5690d1dfb742386f822e0eea52ac06500eccb
IreneBioque <irenebioque@gmail.com>
2021-09-24 08:51:06 +0200

    Publish in GitHub Pages

---
+++ docs/static/js/main.e1f4985d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcontact-list"]=this["webpackJsonpcontact-list"]||[]).push([[0],{10:function(t,n,c){"use strict";c.r(n);c(1);var e=c(3),o=c.n(e),s=(c(8),c(0));var i=function(){return Object(s.jsx)("div",{children:Object(s.jsx)("h1",{children:"Hola mundo!"})})};o.a.render(Object(s.jsx)(i,{}),document.getElementById("root"))},8:function(t,n,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.e1f4985d.chunk.js.map
