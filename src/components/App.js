8b1f4ddfbdacd1865364af54887ca40d1a7a354f
IreneBioque <irenebioque@gmail.com>
2021-09-24 09:03:28 +0200

    add estructure html

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,21 @@ import '../styles/App.scss';
 function App() {
   return (
     <div>
-      <h1>Hola mundo!</h1>
+      <h1>Mis clubs</h1>
+      <ul></ul>
+      <form className='form'>
+        <h2>Añadir un nuevo club</h2>
+        <label>
+          Nombre del club <input type='text' />
+        </label>
+        <label>
+          ¿Abre entre semana? <input type='checkbox' />
+        </label>
+        <label>
+          ¿Abre los fines semana? <input type='checkbox' />
+        </label>
+        <button className='button'>Añadir un nuevo club</button>
+      </form>
     </div>
   );
 }
