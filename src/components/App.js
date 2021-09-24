12ff24d5d4b9c287571ca798bf71445cadfe0871
IreneBioque <irenebioque@gmail.com>
2021-09-24 08:48:52 +0200

    add react starter kit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+// import { useState } from 'react';
+import '../styles/App.scss';
+// import data from './data.json';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola mundo!</h1>
+    </div>
+  );
+}
+
+export default App;
